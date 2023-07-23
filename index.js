d54591c30fd78706682d1751165c01fff28a8e8e
Bohdan Hora <bohdan.hora@intenselab.com>
2023-07-23 21:10:51 +0300

    Register method

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js';
 
 mongoose
     .connect(
-        'mongodb+srv://cocobohd:wwwwww@cluster0.1cvuhkq.mongodb.net/?retryWrites=true&w=majority'
+        'mongodb+srv://cocobohd:wwwwww@cluster0.1cvuhkq.mongodb.net/blog?retryWrites=true&w=majority'
     )
     .then(() => console.log('DB: OK!'))
     .catch((err) => console.log('DB error', err));
@@ -13,25 +19,48 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
 
-    const token = jwt.sign(
-        {
+        const doc = new UserModel({
             email: req.body.email,
-            fullName: 'Bohdan Hora',
-        },
-        'hash123'
-    );
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
 
-    res.json({
-        success: true,
-        token,
-    });
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'id1800',
+            {
+                expiresIn: '30d',
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Can`t register',
+        });
+    }
 });
 
 app.listen(5555, (err) => {
